497480aa7115e29032e3399d803a57a23f029eea
ch450 <ch450@duke.edu>
2022-04-01 20:35:29 -0400

    docker incomplete

--- src/run.sh
+++ src/run.sh
@@ -1,10 +1,1 @@
-sudo -u user_name postgres "MARKET"
-ALTER USER user_name WITH PASSWORD 'passw0rd';
-\q
-exit
-sudo service postgresql restart
-psql -U postgres
-
-
-
 taskset --cpu-list 0-3 python3 server.py
